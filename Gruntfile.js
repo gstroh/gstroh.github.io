cd314d0d20a9f5fc11319f29a6a199ee02a4e0f7
Gregg Strohkorb <greggstrohkorb@Greggs-MacBook-Pro-10.local>
2016-02-11 09:44:36 -0600

    Added use of htmlmin in grunt to minimize html files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,11 +71,7 @@ module.exports = function(grunt) {
         collapseWhitespace: true
         },
         files: {                                   // Dictionary of files
-          'dist/index.html': 'src/index.html',     // 'destination': 'source'
-          'dist/project-2048.html': 'src/project-2048.html',
-          'dist/project-mobile.html': 'src/project-mobile.html',
-          'dist/project-webperf.html': 'src/project-webperf.html',
-          'dist/views/pizza.html': 'src/views/pizza.html'
+          'dist/index.html': 'src/index.html'      // 'destination': 'source'
         }
       }
     },
